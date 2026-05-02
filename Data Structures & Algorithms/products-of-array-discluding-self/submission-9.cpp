fe14d5b91ec5f0f6ab78512dc21ad1738a6e95bc
milk179 <166411506+milk179@users.noreply.github.com>
2026-05-02 19:14:59 -0400

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        vector<int> result; 
+        vector<int> prefix(nums.size(), 1);
+        vector<int> postfix(nums.size(), 1);
+
+        for (int i = 1; i < nums.size(); i++) prefix[i] = prefix[i - 1] * nums[i - 1];
+
+        for (int i = nums.size() - 2; i >= 0; i--) postfix[i] = postfix[i + 1] * nums[i + 1];
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            result.push_back(prefix[i] * postfix[i]);
+        }
+
+        return result;
+    }
+};
