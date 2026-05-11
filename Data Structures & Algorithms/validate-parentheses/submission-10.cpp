0391f1f2d15cf862972e059e7457536646b1f247
milk179 <166411506+milk179@users.noreply.github.com>
2026-05-11 14:02:51 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> sk;
+
+        if (s.size() <= 1) return false;
+        
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s.at(i) == '(' || s.at(i) == '[' || s.at(i) == '{') sk.push(s.at(i));
+            else if ((s.at(i) == ')' || s.at(i) == ']' || s.at(i) == '}') && !sk.empty()) {
+
+                if (sk.top() == '(' && s.at(i) == ')') sk.pop();
+                else if (sk.top() == '[' && s.at(i) == ']') sk.pop();
+                else if (sk.top() == '{' && s.at(i) == '}') sk.pop();
+                else return false;
+            }
+            else return false;
+        }
+
+        if (sk.empty()) return true;
+        else return false;
+    }
+};
